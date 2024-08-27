11a80469d8dee3767bd2dc9e061a63659bdf9fb0
SergPix <250pixels@gmail.com>
2024-08-27 20:58:41 +0300

    Initial Commit

---
+++ RemoteRepoTest/RemoteRepoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRepoTestApp.swift
+//  RemoteRepoTest
+//
+//  Created by Serg Kolodny on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RemoteRepoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
